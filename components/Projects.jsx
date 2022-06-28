29fce6428fe9b9a8c066301ecb736217e6d5d70f
Jezdic <jezdicpaladins@gmail.com>
2022-06-28 19:10:03 +0200

    does vercel work

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <div
       id='projects'
-      className='scroll-mt-20 flex flex-col items-center mt-20 md:w-1/2 mx-auto'
+      className='scroll-mt-20 flex flex-col items-center mt-20 md:w-1/2 mx-auto mb-2'
     >
       <h1 className='text-4xl md:text-5xl underline md:mb-20'>Projects</h1>
       <div className='flex flex-wrap gap-10 justify-center'>
